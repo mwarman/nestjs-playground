c0342574982d8f42b3cde4d20fab73586a88f124
Matthew Warman <warmanmr@gmail.com>
2025-09-27 14:42:46 -0400

    Remove AppController, AppService, and related tests to simplify the application structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoggerModule } from 'nestjs-pino';
 
 import { validate } from './config/configuration';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { HealthModule } from './modules/health/health.module';
 
@@ -45,7 +43,7 @@ import { HealthModule } from './modules/health/health.module';
     TasksModule,
     HealthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
